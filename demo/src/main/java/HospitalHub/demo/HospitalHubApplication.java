0b9905bb36a447bc70872ff5fe153de927646553
NemanjaRanitovic <nemanjaranit@gmail.com>
2023-11-03 15:57:24 +0100

    Project initialization
    Hello World controller test.

---
+++ demo/src/main/java/HospitalHub/demo/HospitalHubApplication.java
@@ -0,0 +1,13 @@
+package HospitalHub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalHubApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/HospitalHub/demo/HospitalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package HospitalHub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
