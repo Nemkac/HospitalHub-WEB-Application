4cb42f4e4da5449d00efe55c96cc604dc3421c0d
AndreaaMi <126777292+AndreaaMi@users.noreply.github.com>
2023-11-08 23:48:47 +0100

    Company Administrator Update

--- demo/src/main/java/HospitalHub/demo/repository/CompanyRepository.java
+++ demo/src/main/java/HospitalHub/demo/repository/CompanyRepository.java
@@ -1,13 +1,9 @@
 package HospitalHub.demo.repository;
 
 import HospitalHub.demo.model.Company;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
