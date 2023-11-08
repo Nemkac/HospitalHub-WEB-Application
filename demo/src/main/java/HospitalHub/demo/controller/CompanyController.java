81ce098beb0f0a1655958afb169099996a7e2ef0
AndreaaMi <126777292+AndreaaMi@users.noreply.github.com>
2023-11-08 16:33:35 +0100

    Update company

--- demo/src/main/java/HospitalHub/demo/controller/CompanyController.java
+++ demo/src/main/java/HospitalHub/demo/controller/CompanyController.java
@@ -3,15 +3,18 @@ package HospitalHub.demo.controller;
 import HospitalHub.demo.dto.CompanyDTO;
 import HospitalHub.demo.model.Company;
 import HospitalHub.demo.service.CompanyService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "api/company")
@@ -37,6 +40,41 @@ public class CompanyController {
 
         company = companyService.save(company);
         return new ResponseEntity<>(new CompanyDTO(company), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Integer id){
+        List<Company> companies = companyService.findAll();
+
+        for(Company company : companies){
+            int companyId = company.getId();
+            if(companyId == id){
+                return new ResponseEntity<>(company, HttpStatus.FOUND);
+            }
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(consumes = "application/json", value = "/update/{id}")
+    public ResponseEntity<CompanyDTO> updateCompany(@RequestBody CompanyDTO companyDTO, @PathVariable Integer id)
+    {
+        Company company = companyService.getById(id);
+
+        if (company == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // Ažurirajte podatke kompanije
+        company.setName(companyDTO.getName());
+        company.setCity(companyDTO.getCity());
+        company.setCountry(companyDTO.getCountry());
+        company.setAvgRate(companyDTO.getAvgRate());
+
+        // Sačuvajte ažuriranu kompaniju
+        company = companyService.save(company);
+
+        return new ResponseEntity<>(new CompanyDTO(company), HttpStatus.OK);
+
     }
 
 }
