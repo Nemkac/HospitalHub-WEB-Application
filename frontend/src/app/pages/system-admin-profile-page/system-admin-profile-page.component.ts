8a5b1f71ab35bd3f843abcf3e4dadbfd9173220a
Nemkac <nemanjatodorovic132002002@gmail.com>
2023-11-15 00:30:01 +0100

    Forms display reorganization

---
+++ frontend/src/app/pages/system-admin-profile-page/system-admin-profile-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-admin-profile-page',
+  templateUrl: './system-admin-profile-page.component.html',
+})
+export class SystemAdminProfilePageComponent {
+
+}
