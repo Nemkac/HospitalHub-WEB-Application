b76e0fbd6a7c92924a3caaf05474f6bf86a861c4
blanusa <vlada2002blanusa@gmail.com>
2023-11-19 23:46:34 +0100

    PC -> Laptop package changes

--- demo/src/main/resources/data-postgres.sql
+++ demo/src/main/resources/data-postgres.sql
@@ -2,8 +2,8 @@ insert into company (avg_rate, id, city, country, name)
     values (0.0, 1, 'Sremska Mitrovica', 'Serbia', 'Andrea'),
            (2.0,2,'Kragujevac','Serbia','Kompa1'),
            (3.0,3,'Sombor','Serbia','Kompa2'),
-           (4.0,4,'Kitograd','Mongolija','Kompa3');
+           (4.0,4,'Gradograd','Mongolija','Kompa3');
 
 
 insert into users (id,name,last_name,password,date_of_birth,email,telephone,country,city,profession,company_info,enabled)
-    values (1,'vladimir','blanusa','123','1-1-1010','vb@gm','381','srbija','krag','profesn','najjaca firmetino bajo','true')
+    values (1,'Vladimir','Blanusa','tastatura','01-26-2002','vlada2002blanusa@gmail.com','+381643217393','Serbia','Kragujevac','Director','Kompani info','true')
