ebed1e7a0bf2d619ee721aacac610f3ab690a226
Nemkac <nemanjatodorovic132002002@gmail.com>
2023-11-29 23:20:29 +0100

    Post-merge navbar fix

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,44 +3,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LogInDTO } from "src/LogInDTO";
-import { User } from "src/User";
 import { catchError, map ,tap} from 'rxjs/operators';
+import { User } from 'src/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiServerUrl = 'http://localhost:8081';
+    private apiServerUrl = 'http://localhost:8081';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  public updateCompanyAdministrator(userDTO: UserDTO, id : number): Observable<UserDTO> {
-      console.log("Update Company Administrator: ", userDTO);  
-      return this.http.put<UserDTO>(`${this.apiServerUrl}/api/user/update/${id}`, userDTO);
-  }
+    public updateCompanyAdministrator(userDTO: UserDTO, id : number): Observable<UserDTO> {
+        console.log("Update Company Administrator: ", userDTO);  
+        return this.http.put<UserDTO>(`${this.apiServerUrl}/api/user/update/${id}`, userDTO);
+    }
 
+    
+    private _loggedInUser?: String = "LOGIN";
+
+    get loggedInUser(): String {
+        return this._loggedInUser as String;
+    }
+    set loggedInUser(user: String) {
+        this._loggedInUser = user;
+    }
   
-  private _loggedInUser?: String = "Log in";
-
-  get loggedInUser(): String {
-      return this._loggedInUser as String;
-  }
-  set loggedInUser(user: String) {
-      this._loggedInUser = user;
-  }
-  
-  public logIn(logInDTO: LogInDTO):Observable<LogInDTO>{      
-      return this.http.post<LogInDTO>(`${this.apiServerUrl}/logIn`, logInDTO).pipe(
+    public logIn(logInDTO: LogInDTO):Observable<LogInDTO>{      
+        return this.http.post<LogInDTO>(`${this.apiServerUrl}/logIn`, logInDTO).pipe(
           tap((response:LogInDTO)=>{
               localStorage.setItem('token', response.email);
-          }
-          )
-      )
-  }
+        })
+    )}
  
-  public register(userDto:User):Observable<User>{
-      return this.http.post<User>(`${this.apiServerUrl}/register`, userDto);
-  }
-
-
+    public register(userDto:User):Observable<User>{
+        return this.http.post<User>(`${this.apiServerUrl}/register`, userDto);
+    }
 }
